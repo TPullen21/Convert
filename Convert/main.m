2f4be5a2478d6720033e9161b180301acb39c1a2
Tom Pullen <tpullen21@gmail.com>
2015-05-12 21:16:46 +0100

    Initial Commit

---
+++ Convert/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Convert
+//
+//  Created by Tom Pullen on 12/05/2015.
+//  Copyright (c) 2015 Tom Pullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
